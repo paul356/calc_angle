141592d4da6184cf350092f6bcad9336f064b637
hao pan <panhao356@gmail.com>
2016-01-25 22:15:24 +0800

    Record the strokes and post it to backend server

--- src/cljs/handwrite_strokes/core.cljs
+++ src/cljs/handwrite_strokes/core.cljs
@@ -1,6 +1,8 @@
 (ns handwrite_strokes.core
   (:require [goog.dom :as dom]
-            [goog.graphics :as graphics]))
+            [goog.graphics :as graphics]
+            [clojure.string :as string]
+            [ajax.core :refer [GET POST]]))
 
 (enable-console-print!)
 
@@ -9,9 +11,12 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce mouse-down (atom false))
-(defonce strokes [])
+(defonce strokes (atom []))
+(defonce curr-stroke (atom "["))
 
 (defn handle-mouseup [evt]
+  (swap! strokes (fn [arr] (conj arr (str @curr-stroke "]"))))
+  (swap! curr-stroke (fn [_] "["))
   (swap! mouse-down (fn [_] false)))
 
 (defn handle-mousedown [evt]
@@ -19,7 +24,13 @@
 
 (defn handle-mousemove [evt]
   (when @mouse-down 
-    (.log js/console (str "(" (.-offsetX evt) "," (.-offsetY evt) ")"))))
+    (swap! curr-stroke (fn [curr-str] (str curr-str (str "'(" (.-offsetX evt) " " (.-offsetY evt) ") "))))))
+
+(defn format-strokes []
+  (str "{" :scale " " (.-width (.getElementById js/document "canvas")) " " :strokes " [" (string/join " " @strokes) "]}"))
+
+(defn call-set-strokes []
+  (POST "/write-character" {:param {:strokes (format-strokes)}}))
 
 (defn start []
   (let [canvas2d (.getElementById js/document "canvas")
@@ -34,6 +45,6 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  (start))
+  )
 
 (set! (.-onload js/window) start)
